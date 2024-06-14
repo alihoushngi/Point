02c28c2f2ea59c1692055ded878ab9984bc2a967
alihoushngi <blumy.info@fmail.com>
2024-06-14 22:32:41 +0330

    navbar and header update

--- src/components/Base/Header/Header.tsx
+++ src/components/Base/Header/Header.tsx
@@ -3,8 +3,8 @@ import React from "react";
 const Header = () => {
   return (
     <header className="flex flex-col justify-center items-center min-h-[calc(100vh-200px)] mt-20">
-      <div className="relative top-0 left-0 right-0 box-border w-[300px] h-[300px] rounded-full flex justify-center items-center">
-        <h1 className="text-center text-3xl">
+      <div className="relative top-0 left-0 right-0 box-border w-[400px] h-[400px] rounded-full flex justify-center items-center">
+        <h1 className="text-center text-4xl">
           Take Your Business <br /> To The Next Level
         </h1>
         <div className="circle-wave1 absolute w-[300px] h-[300px] border border-secondary rounded-full"></div>
